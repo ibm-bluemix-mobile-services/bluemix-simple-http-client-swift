e77737f77b3451366cb5d9b135ec172b42557835
Anton Aleksandrov <antona@us.ibm.com>
2016-04-28 16:20:37 -0500

    Made  HttpError comparable Int

--- Sources/HTTPSClient.swift
+++ Sources/HTTPSClient.swift
@@ -5,37 +5,30 @@ import HTTPSClient
 public typealias NetworkRequestCompletionHandler = (error:HttpError?, data:NSData?, status:Int?, headers: [String:String]?) -> Void
 internal let NOOPNetworkRequestCompletionHandler:NetworkRequestCompletionHandler = {(a,b,c,d)->Void in}
 
-class test{
-	func a(){
-		
-	}
-}
-
-
 ///	Used to indicate various failure types that might occur during HTTPS operations
-public enum HttpError: ErrorProtocol{
+public enum HttpError: Int, ErrorProtocol{
 	/**
 	Indicates a failure during connection attempt. Since response and data is not available in this case an error message might be provided
-	
+
 	- Parameter message: An optional description of failure reason
 	*/
-	case ConnectionFailure(message:String?)
+	case ConnectionFailure = 1
 
 	/// Indicates a resource not being available on server. Returned in case of HTTP 404 status
-	case NotFound
+	case NotFound = 2
 
 	/// Indicates a missing authorization or authentication failure. Returned in case of HTTP 401 status
-	case Unauthorized
+	case Unauthorized = 3
 
 	/// Indicates an error reported by server. Retruned in cases of HTTP 4xx and 5xx statuses which are not handled separately
-	case ServerError
-	
+	case ServerError = 4
+
 	/// Indicates an invalid Uri passed to the BluemixHTTPSClient
-	case InvalidUri
-	
+	case InvalidUri = 5
+
 	/// Indicates that HTTP client was unable to send request
-	case InvalidRequest
-	
+	case InvalidRequest = 6
+
 }
 
 public class HTTPSClient{
@@ -45,22 +38,22 @@ public class HTTPSClient{
 	public class func get(url:String, headers:[String:String]? = nil, completionHandler:NetworkRequestCompletionHandler = NOOPNetworkRequestCompletionHandler){
 		HTTPSClient.sendRequest(url: url, method: .get , headers: headers, completionHandler: completionHandler)
 	}
-		
+
 	/// Send a PUT request
 	public class func put(url:String, headers:[String:String]? = nil, data:NSData?, completionHandler:NetworkRequestCompletionHandler = NOOPNetworkRequestCompletionHandler){
 		HTTPSClient.sendRequest(url: url, method: .put , headers: headers, data: data, completionHandler: completionHandler)
 	}
-		
+
 	/// Send a DELETE request
 	public class func delete(url:String, headers:[String:String]? = nil, completionHandler:NetworkRequestCompletionHandler = NOOPNetworkRequestCompletionHandler){
 		HTTPSClient.sendRequest(url: url, method: .delete , headers: headers, completionHandler: completionHandler)
 	}
-		
+
 	/// Send a POST request
 	public class func post(url:String, headers:[String:String]? = nil, data:NSData?, completionHandler:NetworkRequestCompletionHandler = NOOPNetworkRequestCompletionHandler){
 		HTTPSClient.sendRequest(url: url, method: .post , headers: headers, data: data, completionHandler: completionHandler)
 	}
-	
+
 	/// Send a HEAD request
 	public class func head(url:String, headers:[String:String]? = nil, completionHandler:NetworkRequestCompletionHandler = NOOPNetworkRequestCompletionHandler){
 		HTTPSClient.sendRequest(url: url, method: .head , headers: headers, completionHandler: completionHandler)
@@ -71,7 +64,7 @@ extension HTTPSClient{
 
 	/**
 	Send a request
-	
+
 	- Parameter url: The URL to send request to
 	- Parameter method: The HTTP method to use
 	- Parameter contentType: The value of a 'Content-Type' header
@@ -84,18 +77,18 @@ extension HTTPSClient{
 		guard requestUri != nil else {
 			return completionHandler(error: HttpError.InvalidUri, data: nil, status: nil, headers: nil)
 		}
-		
+
 		if requestUri!.port == nil{
 			requestUri!.port = requestUri?.scheme == "https" ? 443 : 80
 		}
-		
+
 		let client = try? Client(uri:requestUri!)
 		guard client != nil else {
 			return completionHandler(error: HttpError.InvalidUri, data: nil, status: nil, headers: nil)
 		}
-		
+
 		var s4headers = S4.Headers()
-		
+
 		if let headers = headers {
 			for (name, value) in headers{
 				var s4header = S4.Header()
@@ -103,10 +96,10 @@ extension HTTPSClient{
 				s4headers.headers.updateValue(s4header, forKey: CaseInsensitiveString(name))
 			}
 		}
-		
+
 		let response:S4.Response?;
 		var byteArray:[UInt8] = []
-		
+
 		if let data = data {
 			let dataLength = data.length
 			let bytesCount = dataLength/sizeof(UInt8)
@@ -116,15 +109,15 @@ extension HTTPSClient{
 		} else {
 			response = try? client!.send(method: method, uri: requestUri!.path!, headers: s4headers)
 		}
-		
-		
+
+
 		if var response = response {
 			let statusCode = response.status.statusCode
 			let s4headers = response.headers
 			let responseBodyByteArray = try? response.body.becomeBuffer()
 			let responseBodyByteArraySize = responseBodyByteArray?.bytes.count
 			let responseBodyData = NSData(bytes: responseBodyByteArray?.bytes, length: responseBodyByteArraySize!)
-			
+
 			switch statusCode {
 			case 401:
 				logger.error(String(HttpError.Unauthorized))
@@ -148,14 +141,14 @@ extension HTTPSClient{
 				completionHandler(error: nil, data: responseBodyData, status: statusCode, headers: convertHeaders(s4headers: s4headers))
 				break
 			}
-			
+
 			// var bodyString = String(bytes: bodyByteArray!, encoding: NSUTF8StringEncoding)
-			
+
 		} else {
 			completionHandler(error: HttpError.InvalidRequest, data: nil, status: nil, headers: nil)
 		}
 	}
-	
+
 	private class func convertHeaders(s4headers:S4.Headers) -> [String:String]{
 		var headers:Dictionary<String, String> = [:]
 		for (key, value) in s4headers{
@@ -165,4 +158,3 @@ extension HTTPSClient{
 	}
 
 }
-
