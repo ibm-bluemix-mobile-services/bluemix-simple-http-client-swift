435b2cac5963a3d142e70900862e0f50fb93b9f9
Anton Aleksandrov <antona@us.ibm.com>
2016-04-28 15:31:54 -0500

    updated simpelogger dependency

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "BluemixHTTPSClient",
 	dependencies:[
-		.Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simplelogger.git", majorVersion: 0),
+		.Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simplelogger-swift.git", majorVersion: 0),
 		.Package(url: "https://github.com/VeniceX/HTTPSClient.git", majorVersion: 0, minor: 5),
 	]
 )
